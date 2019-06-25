5f7212f193ad1266332bc44e00cf1fd6170f7bc5
rycgar <denis.priss@disy.net>
2019-06-25 11:53:34 +0200

    Extend RichFunction with jQuerySelector() Extend README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # richfaces-util
 
-This is a fork of jboss RichFunction#[clientId](https://docs.jboss.org/richfaces/4.3.X/4.3.0.Final/javadoc/richfaces-components-ui/org/richfaces/function/RichFunction.html#clientId(java.lang.String)).
+This is a fork of jboss
+- RichFunction#[clientId](https://docs.jboss.org/richfaces/4.3.X/4.3.0.Final/javadoc/richfaces-components-ui/org/richfaces/function/RichFunction.html#clientId(java.lang.String)).
+- RichFunction#[jQuerySelector](https://docs.jboss.org/richfaces/4.3.X/4.3.0.Final/javadoc/richfaces-components-ui/org/richfaces/function/RichFunction.html#jQuerySelector(java.lang.String)).
--- src/main/java/net/disy/oss/richfaces/util/RichFunction.java
+++ src/main/java/net/disy/oss/richfaces/util/RichFunction.java
@@ -1,5 +1,6 @@
 package net.disy.oss.richfaces.util;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 import javax.faces.component.NamingContainer;
@@ -11,6 +12,11 @@ import javax.faces.context.FacesContext;
  *
  */
 public class RichFunction {
+  // there is the same pattern in client-side code:
+  // richfaces.js - RichFaces.escapeCSSMetachars(s)
+  private static final char[] CSS_SELECTOR_CHARS_TO_ESCAPE = createSortedCharArray(
+      "#;&,.+*~':\"!^$[]()=>|/");
+
   private RichFunction() {}
 
   /**
@@ -107,5 +113,80 @@ public class RichFunction {
     }
 
     return target;
+  }
+
+  /**
+   * The rich:jQuerySelector('id') function will perform nearly the same function as rich:clientId('id') but will transform
+   * the resulting id into a jQuery id selector which means that it will add a "#" character at the beginning and escape all
+   * reserved characters in CSS selectors.
+   */
+  public static String jQuerySelector(String id) {
+    FacesContext facesContext = FacesContext.getCurrentInstance();
+    UIComponent component = findComponent(facesContext, id);
+    if (component != null) {
+      return jQuerySelector(facesContext, component);
+    }
+
+    return null;
+  }
+
+  /**
+   * Utility method which finds component's jQuery selector based on component's clientId.
+   */
+  private static String jQuerySelector(FacesContext facesContext, UIComponent component) {
+    if (facesContext == null) {
+      throw new IllegalArgumentException("facesContext can't be null");
+    }
+    if (component == null) {
+      throw new IllegalArgumentException("component can't be null");
+    }
+    String clientId = component.getClientId(facesContext);
+    return "#" + escapeCSSMetachars(escapeCSSMetachars(clientId));
+  }
+
+  /**
+   * <p>
+   * Escapes CSS meta-characters in string according to <a href="http://api.jquery.com/category/selectors/">jQuery
+   * selectors</a> document.
+   * </p>
+   *
+   * @param s {@link String} to escape meta-characters in
+   * @return string with escaped characters.
+   */
+  private static String escapeCSSMetachars(String s) {
+    if (s == null || s.length() == 0) {
+      return s;
+    }
+
+    StringBuilder builder = new StringBuilder();
+
+    int start = 0;
+    int idx = 0;
+
+    int length = s.length();
+
+    for (; idx < length; idx++) {
+      char c = s.charAt(idx);
+
+      int searchIdx = Arrays.binarySearch(CSS_SELECTOR_CHARS_TO_ESCAPE, c);
+      if (searchIdx >= 0) {
+        builder.append(s.substring(start, idx));
+
+        builder.append("\\");
+        builder.append(c);
+
+        start = idx + 1;
+      }
+    }
+
+    builder.append(s.substring(start, idx));
+
+    return builder.toString();
+  }
+
+  private static char[] createSortedCharArray(String s) {
+    char[] cs = s.toCharArray();
+    Arrays.sort(cs);
+    return cs;
   }
 }
